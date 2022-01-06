6331edc2b58d5f046871a841054f895ace7abab7
Bi Kong <94787970+WebManager0920@users.noreply.github.com>
2022-01-06 10:24:17 -0800

    update
    q

--- src/components/fetch/fetch.detail.ts
+++ src/components/fetch/fetch.detail.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 const apikey = process.env.REACT_APP_APIKEY;
 const address = process.env.REACT_APP_ADDRESS;
 const contractaddress = process.env.REACT_APP_CONTRACTADDRESS;
-
+const BIKOaddress = '0x4e46a2c7b7d36be9d1461af80cfa1817042ca8d1';
 
 export const fetchDetail = async(current_page:number) => {
     
@@ -13,7 +13,7 @@ export const fetchDetail = async(current_page:number) => {
         let res : any;                            
             res = await axios.get(`https://api-ropsten.etherscan.io/api?module=account&action=tokentx&contractaddress`+contractaddress+`&address=`+address+`&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=`+apikey+``).then((d:any)=>{    
 
-            transaction_data = d.data.result;                       
+            transaction_data = d.data.result.filter((d:any)=>d.contractAddress==BIKOaddress);                       
         });
         
         return transaction_data.reverse().slice(page, limit);        
@@ -29,10 +29,10 @@ export const fetchBlockLength = async() => {
     try {
         let res : any;        
         res = await axios.get(`https://api-ropsten.etherscan.io/api?module=account&action=tokentx&contractaddress`+contractaddress+`&address=`+address+`&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=`+apikey+``).then((d:any)=>{                       
-            block_length = d.data.result.length;
+            block_length = d.data.result.filter((d:any)=>d.contractAddress==BIKOaddress);
             
         });
-        return block_length;
+        return block_length.length;
     } catch (error) {        
     }
 }
