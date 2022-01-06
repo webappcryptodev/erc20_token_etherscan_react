51856ea7ecd918c28723922721bc09dc1399679d
Bi Kong <94787970+WebManager0920@users.noreply.github.com>
2022-01-06 12:41:32 -0800

    c
    c

--- src/components/etherscan/etherscan.table.tsx
+++ src/components/etherscan/etherscan.table.tsx
@@ -39,7 +39,7 @@ export default function EtherscanTable() {
       
       trx = await fetchDetail(currentPage);
       setState(trx);
-    }, 10000);
+    }, 3000);
     return () => clearInterval(data);
   }, [currentPage])
 
